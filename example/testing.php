2f6fb3a9543d947eb0c77261c9ca870b1dc7d141
Gauthier Delamarre <gauthier@objective-php.org>
2016-07-13 19:18:11 +0200

    Minor fixes

--- example/testing.php
+++ example/testing.php
@@ -13,23 +13,33 @@ use Pricer\Logger\Client\Logger;
 
 $start_time = microtime(true);
 
-$logger = new Logger([Logger::PARAMETER_BASEURL =>'http://localhost:8080/']);
+$logger = new Logger([Logger::PARAMETER_BASEURL =>'http://logger.test.flash-global.net']);
+// $logger = new Logger([Logger::PARAMETER_BASEURL =>'http://localhost:8080']);
 $logger->setTransport(new Fei\ApiClient\Transport\BasicTransport());
 
 $notification = new Notification();
 $notification->setMessage('Hello World!');
-$notification->setLevel(Notification::DEBUG);
+$notification->setLevel(Notification::LVL_DEBUG);
 $notification->setCategory(Notification::PERFORMANCE);
-
+$notification->setContext(array
+(
+   "type" => 8,
+   "message" => "Undefined variable: a",
+   "file" => "C:\WWW\index.php",
+   "line" => 2
+)
+);
 /** @var \Fei\ApiClient\ResponseDescriptor $log */
 $log = $logger->notify($notification);
 
 $end_time = microtime(true);
-
+    
+    
 if($log instanceof \Fei\ApiClient\ResponseDescriptor){
+    print_r((string) $log->getBody());
     echo('Response '. $log->getCode(). PHP_EOL);
 }else{
-    echo "An error occured.".PHP_EOL;
+    echo "An error occurred.".PHP_EOL;
 }
 
 echo "time: ", bcsub($end_time, $start_time, 2), "\n";
--- src/Logger.php
+++ src/Logger.php
@@ -1,117 +1,135 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Neofox
- * Date: 21/06/2016
- * Time: 13:42
- */
-
-namespace Pricer\Logger\Client;
-
-
-use Fei\ApiClient\AbstractApiClient;
-use Fei\ApiClient\ApiRequestOption;
-use Fei\ApiClient\RequestDescriptor;
-use Fei\Service\Logger\Entity\Notification;
-
-class Logger extends AbstractApiClient implements LoggerInterface
-{
-    /** @var  int */
-    protected $filterLevel;
-
-    /** @var  string */
-    protected $exceptionLogFile;
-
-    /** @var bool  */
-    protected $haveBackTrace = true;
-
-    const PARAMETER_BASEURL = 'baseUrl';
-    const PARAMETER_FILTER = 'filter';
-    const PARAMETER_BACKTRACE = 'includedBackTrace';
-
     /**
-     * Logger constructor.
-     *
-     * @param array $options
-     *
+     * Created by PhpStorm.
+     * User: Neofox
+     * Date: 21/06/2016
+     * Time: 13:42
      */
-    public function __construct(array $options = array())
-    {
-        $this->exceptionLogFile = '/tmp/logger.log';
-        $this->filterLevel = isset($options[self::PARAMETER_FILTER]) ? $options[self::PARAMETER_FILTER] : Notification::LVL_DEBUG;
-        $this->haveBackTrace = isset($options[self::PARAMETER_BACKTRACE]) ? $options[self::PARAMETER_BACKTRACE] : true;
-        if(isset($options[self::PARAMETER_BASEURL]) ) {
-            $this->setBaseUrl($options[self::PARAMETER_BASEURL]);
-        }
-    }
-
-    /**
-     * @param       $notif
-     * @param array $params
-     *
-     * @return $this|\Fei\ApiClient\ResponseDescriptor
-     */
-    public function notify($notif, array $params = [])
-    {
-        try {
-            if (is_string($notif)) {
-                $notification = new Notification();
-                $notification->setMessage($notif)
-                    ->setLevel(Notification::LVL_INFO)
-                    ->setCategory(Notification::BUSINESS);
-            }else{
-                $notification = $notif;
-            }
-
-            $this->prepareNotification($notification, $params);
-
-            $request = new RequestDescriptor();
-            $request->addBodyParam('message', $notification->getMessage());
-            $request->addBodyParam('context', json_encode($notification->getContext()));
-            $request->addBodyParam('origin', 'http');
-            $request->addBodyParam('level', (int)$notification->getLevel());
-            $request->addBodyParam('namespace', $notification->getNamespace());
-            $request->addBodyParam('server', $this->getServerName());
-            $request->addBodyParam('user', $notification->getUser());
-            $request->addBodyParam('command', $notification->getCommand());
-            $request->addBodyParam('env', $notification->getEnv());
-            $request->addBodyParam('category', $notification->getCategory());
-
-            if ($this->haveBackTrace) {
-                $backtrace = debug_backtrace(DEBUG_BACKTRACE_PROVIDE_OBJECT, 3);
-                unset($backtrace[0]);
-                $request->addBodyParam('backtrace', json_encode($backtrace));
-            }
-
-            $request->setUrl($this->buildUrl('/api/notifications'));
-            $request->setMethod('POST');
-
-            if ($notification->getLevel() >= $this->filterLevel) {
-                return $this->send($request, ApiRequestOption::NO_RESPONSE);
-            }
-
-        } catch (\Exception $e) {
-            file_put_contents($this->exceptionLogFile, $e, FILE_APPEND);
-        }
-
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
-    protected function getServerName()
-    {
-        $uname = posix_uname();
-
-        return $uname['nodename'];
-    }
     
-
-    protected function prepareNotification(Notification $notification, $params = null)
+    namespace Pricer\Logger\Client;
+    
+    
+    use Fei\ApiClient\AbstractApiClient;
+    use Fei\ApiClient\ApiRequestOption;
+    use Fei\ApiClient\RequestDescriptor;
+    use Fei\Service\Logger\Entity\ContextTransformer;
+    use Fei\Service\Logger\Entity\Notification;
+    
+    class Logger extends AbstractApiClient implements LoggerInterface
     {
-        $notification->hydrate($params);
+        const PARAMETER_BASEURL   = 'baseUrl';
+        
+        const PARAMETER_FILTER    = 'filter';
+        
+        const PARAMETER_BACKTRACE = 'includedBackTrace';
+        
+        /** @var  int */
+        protected $filterLevel;
 
-        return $notification;
+        /** @var  string */
+        protected $exceptionLogFile;
+
+        /** @var bool */
+        protected $haveBackTrace = true;
+        
+        /**
+         * Logger constructor.
+         *
+         * @param array $options
+         *
+         */
+        public function __construct(array $options = array())
+        {
+            $this->exceptionLogFile = '/tmp/logger.log';
+            $this->filterLevel      = isset($options[self::PARAMETER_FILTER]) ? $options[self::PARAMETER_FILTER] : Notification::LVL_DEBUG;
+            $this->haveBackTrace    = isset($options[self::PARAMETER_BACKTRACE]) ? $options[self::PARAMETER_BACKTRACE] : true;
+            if (isset($options[self::PARAMETER_BASEURL]))
+            {
+                $this->setBaseUrl($options[self::PARAMETER_BASEURL]);
+            }
+        }
+        
+        /**
+         * @param       $message
+         * @param array $params
+         *
+         * @return $this|\Fei\ApiClient\ResponseDescriptor
+         */
+        public function notify($message, array $params = [])
+        {
+            try
+            {
+                if (is_string($message))
+                {
+                    $notification = new Notification();
+                    $notification->setMessage($message)
+                                 ->setLevel(Notification::LVL_INFO)
+                                 ->setCategory(Notification::BUSINESS)
+                    ;
+                }
+                else
+                {
+                    $notification = $message;
+                }
+                
+                $this->prepareNotification($notification, $params);
+                
+                $context = array();
+                $contextTransformer = new ContextTransformer();
+                foreach ($notification->getContext() as $contextItem)
+                {
+                    $context[$contextItem->getKey()] = $contextItem->getValue();
+                }
+                
+                $request = new RequestDescriptor();
+                $request->addBodyParam('message', $notification->getMessage());
+                $request->addBodyParam('context', json_encode($context));
+                $request->addBodyParam('origin', 'http');
+                $request->addBodyParam('level', (int) $notification->getLevel());
+                $request->addBodyParam('namespace', $notification->getNamespace());
+                $request->addBodyParam('server', $this->getServerName());
+                $request->addBodyParam('user', $notification->getUser());
+                $request->addBodyParam('command', $notification->getCommand());
+                $request->addBodyParam('env', $notification->getEnv());
+                $request->addBodyParam('category', $notification->getCategory());
+                
+                if ($this->haveBackTrace)
+                {
+                    $backtrace = debug_backtrace(DEBUG_BACKTRACE_PROVIDE_OBJECT, 3);
+                    unset($backtrace[0]);
+                    $request->addBodyParam('backtrace', json_encode($backtrace));
+                }
+                
+                $request->setUrl($this->buildUrl('/api/notifications'));
+                $request->setMethod('POST');
+                
+                if ($notification->getLevel() >= $this->filterLevel)
+                {
+                    return $this->send($request, ApiRequestOption::NO_RESPONSE);
+                }
+                
+            } catch (\Exception $e)
+            {
+                file_put_contents($this->exceptionLogFile, $e, FILE_APPEND);
+            }
+            
+            return $this;
+        }
+        
+        protected function prepareNotification(Notification $notification, $params = array())
+        {
+            $notification->hydrate($params);
+            
+            return $notification;
+        }
+        
+        /**
+         * @return string
+         */
+        protected function getServerName()
+        {
+            $uname = posix_uname();
+            
+            return $uname['nodename'];
+        }
     }
-}
